316946e5f01060468f52b46c578d2d3f7a63797e
IsabellaSoares <isabella.soares.28@gmail.com>
2023-09-10 13:47:49 -0300

    feat: create ride type

---
+++ backend/ride/src/types/ride.ts
@@ -0,0 +1,15 @@
+type Ride = {
+    ride_id: string,
+	passenger_id: string,
+	driver_id: string,
+	status: 'requested' | 'completed',
+	fare: Number,
+	distance: Number,
+	from_lat: Number,
+	from_long: Number,
+	to_lat: Number,
+	to_long: Number,
+	date: Date
+}
+
+export default Ride;
